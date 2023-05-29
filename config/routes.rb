9c9621fe7b086c8bc1d385d10557c8b5a60aa5a0
sitarane <julien@larevolution.re>
2023-05-29 22:58:39 +0200

    create user model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :produces
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :produces
 
   # Defines the root path route ("/")
   root "produces#index"
