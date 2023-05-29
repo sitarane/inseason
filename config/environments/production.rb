50c4cad90c2607d893dc344fe8e2174ac3de4b7f
sitarane <julien@larevolution.re>
2023-05-29 22:31:38 +0200

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Devise needs this. There's a similar one in dev.
+  config.action_mailer.default_url_options = { host: 'inseason.world' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
