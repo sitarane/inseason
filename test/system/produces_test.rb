eb55ed05af2d1c2065fa8748024d4658b6b99a6b
sitarane <julien@larevolution.re>
2023-05-29 18:16:23 +0200

    fix the system test

--- test/system/produces_test.rb
+++ test/system/produces_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ProducesTest < ApplicationSystemTestCase
   setup do
-    @produce = produces(:one)
+    @produce = produces(:apple)
   end
 
   test "visiting the index" do
