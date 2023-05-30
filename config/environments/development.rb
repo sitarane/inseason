714c54082ade6a5693d3155332e424d805052850
sitarane <julien@larevolution.re>
2023-05-30 20:00:21 +0200

    add maildev for... mail in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,9 +40,16 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-  
+
   # Required by Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'maildev',
+    :port => 1025,
+    :openssl_verify_mode => 'none'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
