484635a7566feae805df751431d1420c05b0184e
sitarane <julien@larevolution.re>
2023-06-02 23:00:24 +0200

    add boostrap card to Produce view

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -2,6 +2,6 @@ class Produce < ApplicationRecord
   validates :name, presence: true
   has_many :links, dependent: :destroy
   has_one_attached :picture do |picture|
-    picture.variant :thumb, resize_to_limit: [100, 100]
+    picture.variant :thumb, resize_to_limit: [200, 200]
   end
 end
