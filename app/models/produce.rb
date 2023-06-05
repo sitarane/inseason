cf0a2be4941353738d041de09e1a6c2c92365999
sitarane <julien@larevolution.re>
2023-06-05 22:10:45 +0200

    show produces in a grid

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -2,7 +2,7 @@ class Produce < ApplicationRecord
   validates :name, presence: true
   has_many :links, dependent: :destroy
   has_one_attached :picture do |picture|
-    picture.variant :thumb, resize_to_limit: [200, 200]
+    picture.variant :thumb, resize_to_limit: [300, 300]
   end
 
   accepts_nested_attributes_for :links
