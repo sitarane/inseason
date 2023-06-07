dccec315a9a7fd996898b5753c1633cd2fa61962
sitarane <julien@larevolution.re>
2023-06-07 20:32:18 +0200

    add geocoding to seasons

--- app/models/season.rb
+++ app/models/season.rb
@@ -3,4 +3,7 @@ class Season < ApplicationRecord
   validates :latitude, :longitude, :start_time, :end_time, presence: true
   validates :latitude, inclusion: { in: -90..90 }
   validates :longitude, inclusion: { in: -180..180 }
+
+  geocoded_by :place
+  after_validation :geocode
 end
