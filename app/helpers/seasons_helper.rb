fa01fe3d04ce52af0c7f84fd5881706e20326106
sitarane <julien@larevolution.re>
2023-06-16 22:38:26 +0200

    make season form

--- app/helpers/seasons_helper.rb
+++ app/helpers/seasons_helper.rb
@@ -1,2 +1,30 @@
 module SeasonsHelper
+  def seasons_time_pairs
+    [
+      ['Early January'  , 1],
+      ['Late January'   , 3],
+      ['Early February' , 5],
+      ['Late February'  , 8],
+      ['Early March'    , 10],
+      ['Late March'     , 12],
+      ['Early April'    , 15],
+      ['Late April'     , 17],
+      ['Early May'      , 19],
+      ['Late May'       , 22],
+      ['Early June'     , 24],
+      ['Late June'      , 25],
+      ['Early July'     , 27],
+      ['Late July'      , 30],
+      ['Early August'   , 32],
+      ['Late August'    , 35],
+      ['Early September', 37],
+      ['Late September' , 39],
+      ['Early October'  , 41],
+      ['Late October'   , 43],
+      ['Early November' , 45],
+      ['Late November'  , 47],
+      ['Early December' , 49],
+      ['Late December'  , 52]
+    ]
+  end
 end
