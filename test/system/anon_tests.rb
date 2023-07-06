a4d511effc09c0afe89b204a21be1586a94744e6
sitarane <julien@larevolution.re>
2023-07-06 21:45:46 +0200

    fix system test

--- test/system/anon_tests.rb
+++ test/system/anon_tests.rb
@@ -2,16 +2,15 @@ require "application_system_test_case"
 
 class AnonTests < ApplicationSystemTestCase
   test "Basic navigation" do
-    visit root_url
-    assert_selector "h1", text: "Produces"
-    assert_selector "h2", text: "Apple"
-    assert_selector "h2", text: "Carrot"
-    click_on "Apple"
-    assert_selector "h1", text: "Apple"
-    click_on "Back to produces"
-    assert_selector "h1", text: "Produces"
-    assert_selector "h2", text: "Carrot"
-    click_on "Carrot"
-    assert_selector "h1", text: "Carrot"
+    travel_to Time.zone.local(2000, 7, 6) do
+      visit root_url
+      assert_selector "h1", text: "In season now"
+      assert_selector "h2", text: "Apple"
+      click_on "Apple"
+      assert_selector "h1", text: "Apple"
+      click_on "Back to produces"
+      assert_selector "h1", text: "In season now"
+      assert_selector "h2", text: "Apple"
+    end
   end
 end
