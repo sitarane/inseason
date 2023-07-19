a1e2b9780b7ace2cd833ee8c7e9918c423c77579
sitarane <julien@larevolution.re>
2023-07-19 22:19:03 +0200

    add initial i18n config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: %i[ show index ]
   before_action :load_location
 
+  around_action :switch_locale
+
   private
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   def current_location
     session[:location].presence || set_location_from_ip
