d9e5b5b32c26a137f63aa427fe592682f1e9c2aa
sitarane <julien@larevolution.re>
2023-09-03 13:43:34 +0200

    add user agent for OpenStreetMap geolocation

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,6 +2,7 @@ Geocoder.configure(
   # Geocoding options
   timeout: 3,                 # geocoding service timeout (secs)
   lookup: :nominatim,         # name of geocoding service (symbol)
+  http_headers: { "User-Agent" => "InSeason.world, email=julien@larevolution.re" },
   ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   language: :en,              # ISO-639 language code
   use_https: false,           # use HTTPS for lookup requests? (if supported)
