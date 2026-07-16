1c01b2fdf03095aa9c382eefa2243d01ed9bd09e
sitarane <julienboyer@posteo.com>
2026-07-16 18:01:53 +0200

    fix typo

--- app/models/season.rb
+++ app/models/season.rb
@@ -33,7 +33,7 @@ class Season < ApplicationRecord
   end
 
   def ripe?
-    current_week = DateTime.now.c0week # TODO check if timezone-proof
+    current_week = DateTime.now.cweek # TODO check if timezone-proof
     if start_time <= end_time
       return true if current_week.between?(start_time, end_time)
       return false
