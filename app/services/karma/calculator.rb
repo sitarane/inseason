2dee6f557bff663cc7459bfe22b0529e3be08b24
sitarane <julienboyer@posteo.com>
2026-07-28 13:02:22 +0200

    Put all karma incentives into constants

--- app/services/karma/calculator.rb
+++ app/services/karma/calculator.rb
@@ -1,8 +1,11 @@
 module Karma
   class Calculator
-    CONFIRMED_SEASON_KARMA = 3
-    DELETED_SEASON_KARMA = -3
-    DELETED_PRODUCE_KARMA = -1
+    VOUCHED_FOR_CONFIRMED_SEASON = 1
+    VOUCHED_FOR_DELETED_SEASON = -1
+    CREATED_CONFIRMED_SEASON = 3
+    CREATED_DELETED_SEASON = -3
+    CREATED_DELETED_PRODUCE = -1
+    SEASON_IN_CREATED_PRODUCE = 1
 
     def self.calculate_for_user(user)
       score = 0
@@ -12,29 +15,29 @@ module Karma
         season = vouch.season
         if season.present? && !season.destroyed?
           if season.is_confirmed?
-            score += vouch.value ? 1 : -1
+            score += vouch.value ? VOUCHED_FOR_CONFIRMED_SEASON : -VOUCHED_FOR_CONFIRMED_SEASON
           end
         else
           # Season was deleted
-          score += vouch.value ? -1 : 1
+          score += vouch.value ? VOUCHED_FOR_DELETED_SEASON : -VOUCHED_FOR_DELETED_SEASON
         end
       end
 
       # 2. Owned Seasons
       user.seasons.each do |season|
-        score += CONFIRMED_SEASON_KARMA if season.is_confirmed?
+        score += CREATED_CONFIRMED_SEASON if season.is_confirmed?
       end
 
       # 3. Owned Produces
       user.produces.each do |produce|
         produce.seasons.each do |season|
-          score += 1
+          score += SEASON_IN_CREATED_PRODUCE
         end
       end
 
       # 4. Penalties for deletions
-      score += user.seasons_deleted_count.to_i * DELETED_SEASON_KARMA
-      score += user.produces_deleted_count.to_i * DELETED_PRODUCE_KARMA
+      score += user.seasons_deleted_count.to_i * CREATED_DELETED_SEASON
+      score += user.produces_deleted_count.to_i * CREATED_DELETED_PRODUCE
 
       score
     end
